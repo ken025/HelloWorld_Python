a6adad0802be3a27754aba3d4e0eeb8e1fb2b4cb
ken025 <kennethmanuel250@gmail.com>
2021-02-17 22:32:43 -0500

    Worked with strings

--- app.py
+++ app.py
@@ -24,17 +24,68 @@ print(is_published)
 
 # ------ Receiving Input -----
 
-name = input('Enter Your Name: ')
-fav_color = input('Enter Your Favorite Color: ')
-
-print(name + ' likes ' + fav_color)
+# name = input('Enter Your Name: ')
+# fav_color = input('Enter Your Favorite Color: ')
+#
+# print(name + ' likes ' + fav_color)
 
 # ----- Type Conversion -----
-birth_year = input('Birth Year: ')
-print(type(birth_year))
+# birth_year = input('Birth Year: ')
+# print(type(birth_year))
+#
+# age = 2021 - int(birth_year)
+# print(age)
+# print(type(age))
 
-age = 2021 - int(birth_year)
-print(age)
-print(type(age))
+# ----- Strings -----
+
+course = "Python's Course for Beginners"
+print(course)
+
+course_2 = 'Python for "Beginners"'
+print(course_2)
+
+# grabbing characters
+print(course_2[0])
+print(course_2[-1])
+print(course_2[0:3])
+print(course_2[3:])
+print(course_2[:5])
+
+# copying string
+copy = course_2[:]
+print(copy)
+
+# multiple lined strings (triple quotes ''')
+
+greeting = '''
+Hello,
+
+I am testing multi-line strings.
+
+Will it work?
+'''
+print(greeting)
+
+# ----- Formatted Strings ------
+
+first = 'Ken'
+last = 'Colon'
+
+# prefix with f
+mssg = f'{first} [{last}] is a coder'
+print(mssg)
+
+# ------ String Methods -----
+
+# counts characters
+print(len(course_2))
+
+# . opperators
+print(course.upper())
+print(course.find('e'))
+print(course.replace('Beginners', 'Mid-Level'))
+print('for' in course)
+print('For' in course)
 
 
