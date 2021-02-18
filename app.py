897dd50839727c8bd7a86fe4697ef7b34d478d6c
ken025 <kennethmanuel250@gmail.com>
2021-02-17 22:00:40 -0500

    practiced declaring variables

--- app.py
+++ app.py
@@ -3,3 +3,23 @@ print('----------------')
 print('python executes line by line')
 
 print('*' * 10)
+
+# ------ Declaring Variables -------
+
+# integer
+price = 10
+print(price)
+
+# float
+rating = 4.9
+print(rating)
+
+# string
+name = 'Ken'
+print(name)
+
+# boolean
+is_published = False
+print(is_published)
+
+
