ed0660359e9783fefadb0c039f235cf545cf3091
ken025 <kennethmanuel250@gmail.com>
2021-02-18 17:59:00 -0500

    Built guess number game

---
+++ guessing_game.py
@@ -0,0 +1,14 @@
+secret_number = 5
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input('Guess: '))
+    guess_count += 1
+    if guess == secret_number:
+        print("YAY! You won!")
+        # to break loop
+        break
+    else:
+        print('Not Quite')
+else:
+     print('You are out of tries')
